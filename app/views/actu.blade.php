076c5d181442b055448fdaa156cd8899fcaeda3e
QuentinJawad <quanticblob@gmail.com>
2015-12-03 22:12:29 +0100

    faisage de page actu

--- app/views/actu.blade.php
+++ app/views/actu.blade.php
@@ -15,5 +15,7 @@ fjs.parentNode.insertBefore(js,fjs);
 }}(document,"script","twitter-wjs");
 </script>
 </div>
+<div id="maps"></div>
+
 <div "numSecours"><footer>SAMU : 15, Police-secours : 17, Pompiers : 18, Numéro européen d'urgences : 112</footer></div>
 @stop
