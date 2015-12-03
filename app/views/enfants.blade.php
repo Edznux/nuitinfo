00fb4426354d2989ab73ff9c2dcbfcd9bf38ca49
Martinet Antoine <amartin8@scinfe137.univ-pau.fr>
2015-12-03 22:15:46 +0100

    ajouter enfants

---
+++ app/views/enfants.blade.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="enfants.blade.css" />
+			<title>edznux.fr</title>
+</head>
+<body>
+
+<h1>Parce qu'une crise est en cours, rester informé est vital</h1>
+	<figure>
+		<a href="chemin vers la page enfant"><img src="chemin de l'icone enfant" alt="icone pour enfants"/>
+	</figure>
+
+	<figure>
+		<a href="chemin vers la page adulte"><img src="chemin de l'icone adulte" alt="icone de secours"/>
+	</figure>
+
+</body>
+</html>
