f0c08424e0319dd46f107d571345aa519ecf334e
Curryed <nerea.enrique@gmail.com>
2015-12-03 22:55:57 +0100

    Modif messages V3.14

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -11,7 +11,8 @@
 			Si vous avez des événements qui ne sont pas encore sur notre fil d'actualité, n'hésitez pas à nous en faire part. Ici nous sommes à votre écoute.
 
 		</p>	
-		<form id="messages" method="post" action="">
+		{{ Form::open() }}
+    		{{ Form::token() }}
 			Localisation :
 			<select>
 				@foreach ($villes as $ville)
@@ -24,6 +25,6 @@
 			<textarea name="message" rows="5" cols="100">Que ce passe-t-il en ce moment</textarea>
 			</br></br>
 			<input type="Submit" name="envoie" value="Envoyer">
-		</form>
+		{{ Form::close() }}
 	@endif
 @stop
