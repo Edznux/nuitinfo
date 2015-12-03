8e94a89827e3fd701d1659c7658d16db779ceb0f
GuillaumeLeGentil <legoallec.guillaume@hotmail.fr>
2015-12-03 23:49:10 +0100

    css 2

--- app/views/layouts/basic.blade.php
+++ app/views/layouts/basic.blade.php
@@ -5,15 +5,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Nuit Info 2015 uppa</title>
-
-	<link rel="stylesheet" href="/css/main.css">
-	<link rel="stylesheet" href="/css/test.css">
-
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+	
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="stylesheet" href="/css/test.css">
+
+
 
 </head>
 <body>
