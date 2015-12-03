26c385ee6b73d5a62888a974581ff34efd57b94c
Rodolphe <rodolphe.vigneron64@gmail.com>
2015-12-03 22:37:48 +0100

    test V4 lien actif menu

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -14,7 +14,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/map">Map<!--<span class="sr-only">(current)</span>--></a></li>
+        <li ><a href="/map">Map<!--<span class="sr-only">(current)</span>--></a></li>
 		<li><a href="/rules">Règles</a></li>
         <li ><a href="{{{ URL::to( '/actu') }}}" class="list-group-item {{{ (Request::is('/actu') ? 'active' : '') }}}">Actus</a></li>
 		<li><a href="/messages">Messages</a>
