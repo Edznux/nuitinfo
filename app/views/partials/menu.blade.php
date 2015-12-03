f9c00d666393b76f18b86d0ffbc29215bc23119c
Rodolphe <rodolphe.vigneron64@gmail.com>
2015-12-04 00:07:11 +0100

    suppression lien menu inutil V2

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -2,12 +2,12 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </button>-->
+      </button>
       <a class="navbar-brand" href="{{{ URL::to( '/') }}}">Accueil</a>
     </div>
 
