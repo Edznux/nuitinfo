1c02d563c1d1740db25f96ef4a7553dbe5f0b753
edznux <edznux@gmail.com>
2015-12-03 23:25:44 +0100

    add debug

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,9 @@
 var map;
 function initMap() {
+	console.log("init map ");
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 }
+console.log("map ok");
