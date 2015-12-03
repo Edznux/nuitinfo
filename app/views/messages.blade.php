9911277a7be91d3de951f4f1cb424b8a2c743cfc
Curryed <nerea.enrique@gmail.com>
2015-12-03 21:33:33 +0100

    Modif messages V3.1

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-	<h3>Envoie de messages blabla</h3>
+	<h3>Envoie de messages</h3>
 	<p>
 		Si vous avez des événements qui ne sont pas encore sur notre fil d'actualité, n'hésitez pas à nous en faire part. Ici nous sommes à votre écoute.
 
@@ -14,8 +14,11 @@
 		  	<option value="Paris">Paris</option>
 		  	<option value="Bayonne">Bayonne</option>
 		  	<option value="Bordeaux">Bordeaux</option>
-		</select> </br>
+		</select>
+		</br>
 		<button type="button">Ajouter une ville</button>
-		Message : <textarea name="message">Que ce passe t'il en ce moment</textarea>
+		</br>
+		Message : <textarea name="message" rows="20" cols="100">Que ce passe t'il en ce moment</textarea>
+		<input type="Submit" name="envoie" value="Envoyer">
 	</form>
 @stop
