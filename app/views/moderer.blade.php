aeb8ec9cf1167eaf41037701056533071e3792d5
tcampagne <thibault.campagne@gmail.com>
2015-12-04 06:51:07 +0100

    Ajout ae

--- app/views/moderer.blade.php
+++ app/views/moderer.blade.php
@@ -10,10 +10,10 @@
 		{{ Form::open(array('url'=>"/moderer/$message->id") )}}
 			<table width="50%" border=1>
 				<tr> 
-					<td width="25%">{{{$message->message}}} </td>
-					<td width="25%">{{{$message->active ? "activé" : "desactivé" }}} </td>
-					<td width="25%"><input type="submit" name="modifier" value="Modifier"></td>	
-					<td width="25%"><input type="submit" name="supprimer" value="Supprimer"></td>
+					<td width="40%">{{{$message->message}}} </td>
+					<td width="20%">{{{$message->active ? "activé" : "desactivé" }}} </td>
+					<td width="20%"><input type="submit" name="modifier" value="Modifier"></td>	
+					<td width="20%"><input type="submit" name="supprimer" value="Supprimer"></td>
 					
 				</tr>
 			</table>	
