9d753b2c3749d87b93069756ebb42f7283bfff69
Curryed <nerea.enrique@gmail.com>
2015-12-04 05:38:10 +0100

    afficher messages

--- app/views/moderer.blade.php
+++ app/views/moderer.blade.php
@@ -7,20 +7,8 @@
 	<h3>Modérer des messages</h3>
 
 	@foreach($messages as $message)
-		<table width="50%" border=1>
-			<tr> 
-				<td>{{{$message->message}}} </td>
-				{{ Form::open(array('url'=>"/moderer/$message->id") )}}		
-							<td><input type="submit" name="modifier" value="Modifier"></td>
-				{{ Form::close() }}
-
-				{{ Form::open(array('url'=>"/moderer/$message->id") )}}		
-							<td><input type="submit" name="supprimer" value="Supprimer"></td>
-				{{ Form::close() }}
-			</tr>
-		</table>
 		{{ Form::open(array('url'=>"/moderer/$message->id") )}}
-			<table>
+			<table width="50%" border=1>
 				<tr> 
 					<td>{{{$message->message}}} </td>
 					<td>{{{$message->active ? "activé" : "desactivé" }}} </td>
