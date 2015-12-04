9c21dcf1fe9c41c7af91214342e207d1f816d957
GuillaumeLeGentil <legoallec.guillaume@hotmail.fr>
2015-12-04 06:35:39 +0100

    menu3

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -24,12 +24,12 @@
 			<li><a class="menu" href="{{{ URL::to( '/moderer') }}}">Modérer</a>
         @endif
       @endif
-      
+       </ul>
 	   <ul class="nav navbar-nav navbar-right">
 		      <li><a class="menu" href="{{{ URL::to( '/login') }}}">Login</a>
 			  <li><a href="/enfants"><img src="/image/emo-peur.png" class="imageEnfant"/></a></li>
 		</ul>
-		</ul>
+		
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
