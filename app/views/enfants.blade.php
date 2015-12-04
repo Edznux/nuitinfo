15261bf6a2b14b66af4c85cc7bf8e0915684e398
Martinet Antoine <amartin8@scinfe137.univ-pau.fr>
2015-12-04 01:14:58 +0100

    add css for enfant

--- app/views/enfants.blade.php
+++ app/views/enfants.blade.php
@@ -4,10 +4,40 @@
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="enfants.blade.css" />
 			<title>edznux.fr</title>
+
+			
 </head>
 <body>
+	<section id="section" class="main-section first-section">
+			<div class="container">
+				<p class="bigtxt txtcenter">Parce qu'une crise est en cours, rester informé est vital</p>
+			</div>
+	</section>
 
-<h1>Parce qu'une crise est en cours, rester informé est vital</h1>
+	<section id="main" class="main-section cleanbg">
+			<div class="container">
+				<h1>Appeler la police</h1>
+
+				<p>ku</p>
+
+				<p>Su</p>
+
+				<h2>gu</h2>
+
+				<p>puio</p>
+
+				<p>fi</p>
+
+				<h2>yu</h2>
+
+				<p>a</p>
+
+				<p>b</p>
+
+			</div>
+		</section>
+
+<h1></h1>
 	<figure>
 		<a href="chemin vers la page enfant"><img src="chemin de l'icone enfant" alt="icone pour enfants"/>
 	</figure>
