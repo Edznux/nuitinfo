558d1b982153e1c3d46e60154161170a2f8f1b74
ben <benjamin.changala@gmail.com>
2015-12-04 05:02:27 +0100

    o

--- public/js/easterEgg.js
+++ public/js/easterEgg.js
@@ -17,31 +17,3 @@ egg1.addHook(function(){
 //////////////////////////////////////////////////////////::::
 
 
-
-
-document.addEventListener('keydown', function (e){
-    if(e.altKey && e.keyCode == 'A'.charCodeAt(0) ) {
-        e.preventDefault();
-        window.location.href = 'http://ni.edznux.fr/';
-    }  
-        if(e.altKey && e.keyCode == 'M'.charCodeAt(0) ) {
-            e.preventDefault();
-              window.location.href='http://ni.edznux.fr/map';
-        }
-        if(e.altKey && e.keyCode == 'R'.charCodeAt(0) ) {
-            e.preventDefault();
-              window.location.href='http://ni.edznux.fr/rules';
-        } 
-        if(e.altKey && e.keyCode == 'P'.charCodeAt(0) ) {
-            e.preventDefault();
-              window.location.href= 'http://ni.edznux.fr/messages';
-        }
-    if(e.altKey && e.keyCode == 'L'.charCodeAt(0) ) {
-            e.preventDefault();
-              window.location.href= 'http://ni.edznux.fr/login';
-        }
-        if(e.altKey && e.keyCode == 'E'.charCodeAt(0) ) {
-            e.preventDefault();
-              window.location.href= 'http://ni.edznux.fr/enfants';
-        }
-    });
