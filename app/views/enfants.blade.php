ccbd5617b47ff19cc5e941569aa2a75a1ed003af
GuillaumeLeGentil <legoallec.guillaume@hotmail.fr>
2015-12-04 07:22:10 +0100

    men7

--- app/views/enfants.blade.php
+++ app/views/enfants.blade.php
@@ -12,12 +12,58 @@
 	<section id="main" class="main-section cleanbg">
 			<div class="container">
 				<h1>En cas d'urgence :</h1>
-				<img src="/image/guideSurvie.jpg" width="45%"/>
-				<br>
-				<img src="/image/tsunami.jpg" width="45%"/>
-				<br>
-				<img src="/image/gestesSurvie.jpg"/>
-				<img src="/image/defibri.jpg"/>
+				
+				<h3>TERRORISME ET ATTENTAT</h3>
+<p>
+Le terrorisme, c'est lorsqu'une personne cherche à semer la terreur auprès d'un
+groupe de personne, comme par exemple les français, ou encore les habitants de la
+Terre. Pour cela, ces personnes que l'on appelle des terroristes, utilisent des
+armes et peuvent par exemple kidnapper, ou blesser des innocents.
+</p>
+
+<p>
+Un attentat est fait par un terroriste. C'est lorsque des personnes méchantes
+décident de faire du mal à beaucoup de personnes tout d'un coup. Généralement, 
+ils utilisent cette attaque pour passer un message au Président de la République
+par exemple. Ils font ça pour essayer de nous montrer qu'ils sont les plus forts
+mais ce n'est pas vrai.
+</p>
+		<P>
+		Surtout quand ce genre de choses arrivent, il ne faut jamais
+avoir peur. Avoir peur, ça serait faire plaisir aux terroristes et ils gagneraient.
+Pour leur montrer qu'on est les plus forts, il faut continuer à vivre normalement.
+</p>
+
+<h3>PANDEMIE EPIDEMIE</h3>
+<p>
+Une épidémie, c'est quand une maladie touche un très grand nombre de personne 
+et se répend très vite.
+Quand une épidémie touche le monde entier, on appelle ça une pandémie.
+Par exemple, quand un enfant de ta classe a un rhume et qu'ensuite tout le monde 
+est malade, on appelle ça une épidémie.
+</p>
+<p>
+Quand on entend parler de ce genre de chose, il ne faut pas paniquer. Pense bien
+à te laver les mains avant de manger.
+</p>
+<h3>CATASTROPHE NATURELLE</h3>
+<p>
+Ici, aucune personne n'est responsable. C'est la nature qui est en colère. 
+Une grosse tempête, une inondation, ou un volcan, ce sont des catastrophes 
+naturelles.
+</p>
+<h4>TSUNAMI</h4>
+<p>
+Ou Raz-de-Marée, c'est quand une vague géante arrivent de l'océan et détruit tout.
+Ces vagues arrivent très rarement, et généralement après un tremblement de terre.
+</p>
+<h4>SEISME</h4>
+<p>
+Ou tremblement de terre, c'est quand la terre se met à bouger très fort. Elle peut
+même s'ouvrir en deux. On peut le sentir tout autour de la Terre mais généralement
+on ne les sent même pas.</p>
+
+<p> Avec ce genre de problèmes, on ne peut pas toujours protéger les gens et les éviter, mais après ces catastrophes il faut s'aider les uns les autres en allant reconstruire leur maison ou en leur donnant de l'argent : on appelle ça des dons.</p>
 				
 
 			</div>
