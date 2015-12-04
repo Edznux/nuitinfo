6f7e80d585835bbd79e9fc6e6603cc066a16aa53
tcampagne <thibault.campagne@gmail.com>
2015-12-04 04:40:29 +0100

    Ajout

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -7,7 +7,8 @@
 		Si vous avez des événements qui ne sont pas encore sur notre fil d'actualité, n'hésitez pas à nous en faire part. Ici nous sommes à votre écoute.
 
 	</p>	
-	<div class="container">
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 	{{ Form::open() }}
 		Localisation :
 		<select name="ville">
@@ -28,4 +29,5 @@
 		<input type="Submit" name="envoie" value="Envoyer">
 	{{ Form::close() }}
 	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
 @stop
