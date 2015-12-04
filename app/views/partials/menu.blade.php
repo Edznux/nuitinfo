91126c045fd5fd3e86f390dbc69d2b235a5f8035
edznux <edznux@gmail.com>
2015-12-04 02:33:43 +0100

    kuhqszdfpgkhjufqsdklghjdsqflkghdqsfgkdhjlfsqjklghdsf

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -18,7 +18,7 @@
 		    <li><a class="menu" href="{{{ URL::to( '/rules') }}}">Règles</a></li>
        <!-- <li><a class="menu" href="{{{ URL::to( '/actu') }}}">Actus</a></li>-->
         <li><a class="menu" href="{{{ URL::to( '/messages') }}}">Messages</a>
-       @if($user->id)
+       @if($user && $user->id)
 		      <li><a class="menu" href="{{{ URL::to( '/position') }}}">position</a>
         @endif
       </ul>
