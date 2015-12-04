fa80dcf3d7b1ca9b2f3e1430b72c2327c791c122
ben <benjamin.changala@gmail.com>
2015-12-04 04:09:35 +0100

    alt

--- public/js/easterEgg.js
+++ public/js/easterEgg.js
@@ -20,27 +20,27 @@ egg1.addHook(function(){
 
 
 document.addEventListener('keydown', function (e){
-    if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0) ) {
+    if(e.altKey && e.keyCode == 'A'.charCodeAt(0) ) {
         e.preventDefault();
         window.location.href = 'http://ni.edznux.fr/';
     }  
-        if(e.ctrlKey && e.keyCode == 'M'.charCodeAt(0) ) {
+        if(e.altKey && e.keyCode == 'M'.charCodeAt(0) ) {
             e.preventDefault();
               window.location.href='http://ni.edznux.fr/map';
         }
-        if(e.ctrlKey && e.keyCode == 'R'.charCodeAt(0) ) {
+        if(e.altKey && e.keyCode == 'R'.charCodeAt(0) ) {
             e.preventDefault();
               window.location.href='http://ni.edznux.fr/rules';
         } 
-        if(e.ctrlKey && e.keyCode == 'P'.charCodeAt(0) ) {
+        if(e.altKey && e.keyCode == 'P'.charCodeAt(0) ) {
             e.preventDefault();
               window.location.href= 'http://ni.edznux.fr/messages';
         }
-    if(e.ctrlKey && e.keyCode == 'L'.charCodeAt(0) ) {
+    if(e.altKey && e.keyCode == 'L'.charCodeAt(0) ) {
             e.preventDefault();
               window.location.href= 'http://ni.edznux.fr/login';
         }
-        if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0) ) {
+        if(e.altKey && e.keyCode == 'E'.charCodeAt(0) ) {
             e.preventDefault();
               window.location.href= 'http://ni.edznux.fr/enfants';
         }
