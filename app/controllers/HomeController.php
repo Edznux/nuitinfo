c04be905affd7782c7c6bff7caea46d8afc8b474
Curryed <nerea.enrique@gmail.com>
2015-12-04 03:42:42 +0100

    afficher messages + get ()

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends BaseController {
 	{
 		return View::make('actu')
 					->with('position', Position::all())
-					->with('messages', Message::where('active','=',0)->get());
+					->with('messages', Message::where('active','>',0)->get());
 	}
 
 	public function showMap()
