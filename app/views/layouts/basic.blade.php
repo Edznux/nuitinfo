e296a07a9a4e3969c7432bc497396ceb35e46bff
ben <benjamin.changala@gmail.com>
2015-12-04 03:20:43 +0100

    fix

--- app/views/layouts/basic.blade.php
+++ app/views/layouts/basic.blade.php
@@ -13,11 +13,6 @@
 	
 	<link rel="stylesheet" href="/css/main.css">
 <!--	<link rel="stylesheet" href="/css/test.css">-->
-       <script type="text/javascript" src="js/lib-egg.js"></script>
-    <script type="text/javascript" src="js/lib-jQuery.js"></script>
-    <script type="text/javascript" src="js/lib-phaser.min.js"></script>
-    <script type="text/javascript" src="js/easterEgg.js"></script>
-    <script type="text/javascript" src="js/game.js"></script>
 
 
 </head>
