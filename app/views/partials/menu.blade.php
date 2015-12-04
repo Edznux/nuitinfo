1b1976ca23636244e962dce4b51f611789edced1
GuillaumeLeGentil <legoallec.guillaume@hotmail.fr>
2015-12-04 06:29:18 +0100

    menu1

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -27,9 +27,10 @@
       </ul>
 	   <ul class="nav navbar-nav navbar-right">
 		      <li><a class="menu" href="{{{ URL::to( '/login') }}}">Login</a>
-			  <li><a href="/enfants"><img src="/image/emo-peur.png" class="imageEnfant"/></a></li>
+			  
 		</ul>
     </div><!-- /.navbar-collapse -->
+	<a href="/enfants"><img src="/image/emo-peur.png" class="imageEnfant"/></a>
   </div><!-- /.container-fluid -->
 </nav>
 <div class="imagehaut"></div>
