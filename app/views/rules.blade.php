afcc16d46ba83aff3f976df27eef11d2b3371edc
GuillaumeLeGentil <legoallec.guillaume@hotmail.fr>
2015-12-04 01:03:20 +0100

    css mise en page2

--- app/views/rules.blade.php
+++ app/views/rules.blade.php
@@ -30,24 +30,24 @@
 	<h4>Si vous êtes à l’extérieur :</h4>
 	<ul>
 		<li>Sortez  de  votre  véhicule,  il  risque  d’être  emporté  beaucoup  plus  facilement que vous.</li>
-		<li>Eloignez vous de la zone dangereuse et réfugiez vous sur un point haut</li>
+		<li>Eloignez vous de la zone dangereuse et réfugiez vous sur un point haut.</li>
 
 	</ul>
 	
-	<h2>Pendant la crue</h2>
+	<h2>Pendant la crue :</h2>
 	<ul>
-		<li>Tenezvous informé de l’évolution de la crise en écoutant la radio</li>
-		<li>N’évacuez qu’après en avoir reçu la consigne par les autorités en charge de votre sécurité</li>
-		<li>N’allez pas chercher vos enfants à l’école, leurs enseignants s’occupent d’eux</li>
-		<li>Ne téléphonez pas, afin de laisser libres les lignes pour les secours</li>
+		<li>Tenezvous informé de l’évolution de la crise en écoutant la radio.</li>
+		<li>N’évacuez qu’après en avoir reçu la consigne par les autorités en charge de votre sécurité.</li>
+		<li>N’allez pas chercher vos enfants à l’école, leurs enseignants s’occupent d’eux.</li>
+		<li>Ne téléphonez pas, afin de laisser libres les lignes pour les secours.</li>
 	</ul>
 
-	<h2>Après la crue</h2>
+	<h2>Après la crue :</h2>
 	<ul>
-		<li>Aérez les pièces dès que possible</li>
-		<li>Contrôlez les installations électriques et de gaz avant de les rétablir</li>
-		<li>Chauffez dès que possible</li>
-		<li>Renseignez-vous auprès de la Mairie pour connaître les démarches à engager</li>
+		<li>Aérez les pièces dès que possible.</li>
+		<li>Contrôlez les installations électriques et de gaz avant de les rétablir.</li>
+		<li>Chauffez dès que possible.</li>
+		<li>Renseignez-vous auprès de la Mairie pour connaître les démarches à engager.</li>
 	</ul>
 	
 	<h1>En cas de séisme</h1>
@@ -55,8 +55,8 @@
 	<h4>Si vous êtes à l’intérieur d’un bâtiment :</h4>
 
 	<ul>
-		<li>Eloignez-vous des fenêtres</li>
-		<li>Abritez-vous sous un meuble solide, contre un mur ou une colonne porteuse</li>
+		<li>Eloignez-vous des fenêtres.</li>
+		<li>Abritez-vous sous un meuble solide, contre un mur ou une colonne porteuse.</li>
 	</ul>
 	
 	<h4>Si vous êtes à l’extérieur :</h4>
@@ -71,26 +71,26 @@
 	</ul>
 	
 	
-	<h2>Dès le fin de la secousse</h2>
+	<h2>Dès le fin de la secousse :</h2>
 -	<ul>
 		<li>Coupez le gaz, l’électricité et l’eau.</li>
-		<li>Ni flamme, ni étincelle pour éviter les risques d’explosion</li>
+		<li>Ni flamme, ni étincelle pour éviter les risques d’explosion.</li>
 		<li>Evacuez  immédiatement  le  bâtiment  (il  peut  y  avoir  d’autres  secousses)  en 
 		emportant  vos  papiers  personnels,  des  vêtements  chauds  et  les  médicaments 
-		indispensables</li>
-		<li>N’utilisez pas les ascenseurs</li>
-		<li>Ne retournez pas dans un bâtiment avant que celui-ci n’ait été contrôlé</li>
-		<li>Tenez vous informé de l’évolution de la crise en écoutant la radio</li>
+		indispensables.</li>
+		<li>N’utilisez pas les ascenseurs.</li>
+		<li>Ne retournez pas dans un bâtiment avant que celui-ci n’ait été contrôlé.</li>
+		<li>Tenez vous informé de l’évolution de la crise en écoutant la radio.</li>
 		<li>N’allez pas chercher vos enfants à l’école : leurs enseignants s’occupent d’eux.</li>	
 		<li>Ne téléphonez pas afin de laisser libres les lignes pour les secours.</li>
 	</ul>
 
 	<h1>En cas de mouvement de terrain	</h1>
-	<h2>Pendant</h2>
+	<h2>Pendant :</h2>
 	<ul>
-		<li>Fuyez latéralement</li>
-		<li>Réfugiez-vous sur le point haut le plus proche</li>
-		<li>Eloignez-vous de la zone dangereuse et ne revenez pas sur vos pas</li>
+		<li>Fuyez latéralement.</li>
+		<li>Réfugiez-vous sur le point haut le plus proche.</li>
+		<li>Eloignez-vous de la zone dangereuse et ne revenez pas sur vos pas.</li>
 	</ul>
 	
 	<h2>Après</h2>
