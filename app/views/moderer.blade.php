56cf0ddc9599d1f4b68da06c25a528ae30cc3425
tcampagne <thibault.campagne@gmail.com>
2015-12-04 06:48:33 +0100

    merge css

--- app/views/moderer.blade.php
+++ app/views/moderer.blade.php
@@ -10,10 +10,10 @@
 		{{ Form::open(array('url'=>"/moderer/$message->id") )}}
 			<table width="50%" border=1>
 				<tr> 
-					<td>{{{$message->message}}} </td>
-					<td>{{{$message->active ? "activé" : "desactivé" }}} </td>
-					<td><input type="submit" name="modifier" value="Modifier"></td>	
-					<td><input type="submit" name="supprimer" value="Supprimer"></td>
+					<td width="25%">{{{$message->message}}} </td>
+					<td width="25%">{{{$message->active ? "activé" : "desactivé" }}} </td>
+					<td width="25%"><input type="submit" name="modifier" value="Modifier"></td>	
+					<td width="25%"><input type="submit" name="supprimer" value="Supprimer"></td>
 					
 				</tr>
 			</table>	
