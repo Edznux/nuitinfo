6bc75dbbb70ae89168c5e9f6949ee96832145c7e
tcampagne <thibault.campagne@gmail.com>
2015-12-04 05:49:30 +0100

    Ajout

--- app/views/actu.blade.php
+++ app/views/actu.blade.php
@@ -15,7 +15,7 @@
 
 <div id="message" >
 <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
-<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+<div class="col-lg-5 col-md-5 col-sm-5 col-xs-3">
 	<a class="twitter-timeline" href="hashtag/nuitinfo2015" data-widget-id="672511905999798272"  height="100%" width="100%"></a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
 if(!d.getElementById(id)){js=d.createElement(s);
